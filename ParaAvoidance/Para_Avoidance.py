cd2984fdde0699d814057cbfa22f2f64c0178d24
Okou583 <63486800+Okou583@users.noreply.github.com>
2020-08-02 00:33:47 +0900

    Update Para_Avoidance.py

--- ParaAvoidance/Para_Avoidance.py
+++ ParaAvoidance/Para_Avoidance.py
@@ -12,10 +12,54 @@ import traceback
 import numpy as np
 import cv2
 #--- original module ---#
+import GPS
+import gps_navigate
 import Capture
 import ParaDetection
 import pwm_control
 import Other
+
+def land_point_save():
+	try:
+		while True:
+			value = GPS.readGPS()
+			latitude_land = value[1]
+			longitude_land = value[2]
+			time.sleep(1)
+			if latitude_land != -1.0 and longitude_land != 0.0 :
+				break
+	except KeyboardInterrupt:
+		GPS.closeGPS()
+		print("\r\nKeyboard Intruppted, Serial Closed")
+
+	except:
+		GPS.closeGPS()
+		print (traceback.format_exc())
+	return longitude_land,latitude_land
+
+def Parachute_area_judge(longitude_land,latitude_land):
+	try:
+		while True:
+			value = GPS.readGPS()
+			latitude_new = value[1]
+			longitude_new = value[2]
+			print(value)
+			print('longitude = '+str(longitude_new))
+			print('latitude = '+str(latitude_new))
+			time.sleep(1)
+			if latitude_new != -1.0 and longitude_new != 0.0 :
+				break
+	except KeyboardInterrupt:
+		GPS.closeGPS()
+		print("\r\nKeyboard Intruppted, Serial Closed")
+
+	except:
+		GPS.closeGPS()
+		print (traceback.format_exc())
+	direction = gps_navigate.vincenty_inverse(longitude_land,latitude_land,longitude_new,latitude_new)
+	distance = direction["distance"]        
+	return distance
+
 
 def Parachute_Avoidance(flug):
 	#--- There is Parachute arround rover ---#
@@ -41,28 +85,39 @@ def Parachute_Avoidance(flug):
 		#--- finish ---#
 
 if __name__ == '__main__':
+	GPS.openGPS()
 	print("START: Judge covered by Parachute")
+	#--- note GPS data at land point ---#
+	longitude_land,latitude_land = land_point_save()
+	#--- initialize distance ---#
+	global distance
+	distance = 0
 	t2 = time.time()
 	t1 = t2
-	#--- Paracute judge for 60 seconds ---#
+	#--- Paracute judge ---#
+	#--- timeout is 60s ---#
 	while t2 - t1 < 60:
 		Luxflug = ParaDetection.ParaJudge(100)
 		if Luxflug[0] == 1:
 			break
 		t1 =time.time()
+		time.sleep(1)
 	print("START: Parachute avoidance")
-	try:
-		#--- first parachute detection ---#
-		flug, area, photoname = ParaDetection.ParaDetection("/home/pi/photo/photo",320,240,200,10,120)
-		Parachute_Avoidance(flug)
+	while distance <= 5:
+		try:
+			#--- first parachute detection ---#
+			flug, area, photoname = ParaDetection.ParaDetection("/home/pi/photo/photo",320,240,200,10,120)
+			Parachute_Avoidance(flug)
+			distance = Parachute_area_judge(longitude_land,latitude_land)
 
+		except KeyboardInterrupt:
+			print("Emergency!")
+			run = pwm_control.Run()
+			run.stop()
 
-	except KeyboardInterrupt:
-		print("Emergency!")
-		run = pwm_control.Run()
-		run.stop()
+		except:
+			run = pwm_control.Run()
+			run.stop()
+			print(traceback.format_exc())
 
-	except:
-		run = pwm_control.Run()
-		run.stop()
-		print(traceback.format_exc())
+#-------  update at 8/2 am1:00 ------#
